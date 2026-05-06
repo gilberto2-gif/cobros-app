062dc5ec83efa0b1e11d1e0fadb63148b6c09d4a
Grupo Rapid IT <ti@gruporapid.com>
2026-05-06 13:06:18 -0500

    Fix manifest indentation

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Rapid - Estado de Cuenta en Aged Receivable',
+    'version': '18.0.1.1.0',
+    'category': 'Accounting/Accounting',
+    'summary': 'Agrega 2 opciones al caret del reporte Aged Receivable: PDF y Email',
+    'author': 'Grupo Rapid IT',
+    'depends': ['account_reports', 'statement_report'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
