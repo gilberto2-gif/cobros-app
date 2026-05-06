ad138ae9ed597c26d7d998e1ca6c2b289fc4f1ef
Grupo Rapid IT <ti@gruporapid.com>
2026-05-06 12:35:50 -0500

    Add handler para Odoo.sh dev-estado-cuenta

---
+++ aged_receivable_handler.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from odoo import models, _
+
+
+class AgedReceivableCustomHandler(models.AbstractModel):
+    _inherit = "account.aged.receivable.report.handler"
+
+    def _caret_options_initializer(self):
+        caret_options = super()._caret_options_initializer()
+        pdf_option = {
+            "name": _("Descargar PDF Estado de Cuenta"),
+            "action": "caret_option_descargar_pdf_estado_cuenta",
+        }
+        email_option = {
+            "name": _("Enviar Estado de Cuenta"),
+            "action": "caret_option_enviar_pdf_estado_cuenta",
+        }
+        target_keys = ("account.aged.receivable", "account.report.partner", "partner.id")
+        added = False
+        for key in target_keys:
+            if key in caret_options:
+                caret_options[key].append(pdf_option)
+                caret_options[key].append(email_option)
+                added = True
+        if not added:
+            caret_options.setdefault("account.aged.receivable", []).extend(
+                [pdf_option, email_option]
+            )
+        return caret_options
+
+    def _extract_partner_from_params(self, params):
+        partner_id = params.get("res_id") or params.get("partner_id")
+        if not partner_id and params.get("line_id"):
+            for part in str(params["line_id"]).split("-"):
+                if part.isdigit():
+                    partner_id = int(part)
+                    break
+        if partner_id:
+            return self.env["res.partner"].browse(int(partner_id))
+        return self.env["res.partner"]
+
+    def caret_option_descargar_pdf_estado_cuenta(self, options, params):
+        partner = self._extract_partner_from_params(params)
+        if not partner:
+            return False
+        return partner.action_print_pdf()
+
+    def caret_option_enviar_pdf_estado_cuenta(self, options, params):
+        partner = self._extract_partner_from_params(params)
+        if not partner:
+            return False
+        return partner.action_share_pdf()
